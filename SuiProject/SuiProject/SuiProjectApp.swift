477dcf027c4d7d8eb1f12b2e8fbea625b1075a14
Niyaz <sueraetae@mail.ru>
2023-09-03 23:48:49 +0300

    initial commit

---
+++ SuiProject/SuiProject/SuiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuiProjectApp.swift
+//  SuiProject
+//
+//  Created by Александр Троицкий on 03.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
